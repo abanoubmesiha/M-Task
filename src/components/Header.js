e6205ea4cf705baa8ffe162bd94028e217c2b4ce
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-13 23:47:51 +0200

    navbar and sidebar

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <React.Fragment>
+            <ul>
+                <li><a class="active" href="#admin">Admin</a></li>
+            </ul>
+        </React.Fragment>
+    );
+};
+
+export default Header;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <React.Fragment>
+            <div class="sidenav">
+                <a href="#em">Employee Management</a>
+            </div>
+
+        </React.Fragment>
+    );
+};
+
+export default Sidebar;
