37d03b9204d8eacdf2244356fe24719d4199302d
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-14 13:53:48 +0200

    add data js

---
+++ src/components/data.js
@@ -0,0 +1,38 @@
+export const customers = [
+    {
+    code: '001',
+    name: "Mohammed",
+    country: 'KSA',
+    job:'Accountant',
+    phone:'01234567891',
+    image:'Not Available',
+    setting:'Edit and Delete'
+},
+    {
+    code: '002',
+    name: "Ibrahim",
+    country: 'KSA',
+    job:'Cashier',
+    phone:'01234567891',
+    image:'Not Available',
+    setting:'Edit and Delete'
+},
+    {
+    code: '003',
+    name: "Abbas",
+    country: 'KSA',
+    job:'Manager',
+    phone:'01234567891',
+    image:'Not Available',
+    setting:'Edit and Delete'
+},
+    {
+    code: '004',
+    name: "Abbas",
+    country: 'KSA',
+    job:'HR',
+    phone:'01234567891',
+    image:'Not Available',
+    setting:'Edit and Delete'
+},
+];
